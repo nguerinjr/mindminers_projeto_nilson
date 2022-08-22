957dfc39f65ea6eb821c199b165e70566a478715
Nilson Guerin Junior <nguerinjr@gmail.com>
2022-08-22 18:28:21 -0300

    Fazendo commit da versao OO do código

--- analise_operacoes.py
+++ analise_operacoes.py
@@ -1,120 +1,218 @@
-from collections import defaultdict
 import pandas as pd
-from datetime import datetime
-
-# TODO: tentar leitura da web, se não der certo, tentar ler do arquivo (colocar isso no código)
-stocks_df = pd.read_csv('stocks-dataset.csv')
-stocks_df['Data da operação'] = stocks_df['Data da operação'].apply(lambda data_op_str: datetime.strptime(data_op_str, "%Y-%m-%d"))
-stocks_df = stocks_df.sort_values('Data da operação', ignore_index=True)
-acoes = stocks_df['Ação'].unique()
-
-def calc_pm(pm, qm, pc, qc, tc):
-    pm = (pm * qm + pc * qc + tc) / (qm + qc)
-    return pm
-
-def calc_add_qm(qm, qc):
-    qm = qm + qc
-    return qc
-
-def calc_sell_qm(qm, qv):
-    qm = qm - qv
-    return qv
-
-def calc_ra(pv, pm, qv, tv):
-    ra = (pv - pm) * qv - tv
-    return ra
+from typing import List
+from calculadora import *
 
 
-pm = defaultdict(lambda: defaultdict(lambda: -1))
-qm = defaultdict(lambda: defaultdict(lambda: -1))
-ra = defaultdict(lambda: defaultdict(lambda: []))
-pa_acao = defaultdict(lambda: defaultdict(lambda: 0))
-pa = defaultdict(lambda: 0)
-ram = defaultdict(lambda: defaultdict(lambda: [0, 0]))
-cont_mes = defaultdict(lambda: 0)
+def converte_tipo_para_int(
+    df: pd.DataFrame,
+    coluna_str: str,
+    ) -> pd.DataFrame:
 
-for index, op in stocks_df.iterrows():
-    acao = op['Ação']
-    mes = op['Data da operação'].month
-    cont_mes[mes] += 1
+    conversao = lambda tipo_str: TipoOperacao.converte_op_para_int(
+        tipo_str
+    )
+    return df[coluna_str].apply(conversao)
 
-    # sempre calcula o qm
-    # ordenação introduzida no python 3.6 e oficialmente incluida no 3.7
-    def get_value(dict_ref, mes):
-        if dict_ref[mes] < 0:
-            last_moths = list(dict_ref.keys())
-            last_moths.pop(-1)
-            if len(last_moths):
-                mes = last_moths[-1]
+
+def pre_processa_df(dados_df: pd.DataFrame, lista_acoes: ListaAcoes) -> pd.DataFrame:
+    # Faz a conversão de colunas do df para os objetos correspondentes da calculadora
+
+    formato_data_str = "%Y-%m-%d"
+
+    dados_df['Data da operação'] = pd.to_datetime(
+        dados_df['Data da operação'], format=formato_data_str)
+    dados_df = dados_df.sort_values('Data da operação', ignore_index=True)
+    dados_df['Operação'] = converte_tipo_para_int(
+        dados_df, 'Operação'
+    )
+    retorna_acao = lambda a: lista_acoes.retorna_acao_por_nome(a)
+    dados_df['Ação'] = dados_df['Ação'].apply(retorna_acao)
+
+    return dados_df
+
+
+def cria_df_de_operacoes(dados_df: pd.DataFrame) -> List[Operacao]:
+    conversao = lambda e: Operacao(
+        e['Data da operação'],
+        e['Operação'],
+        e['Ação'],
+        e['Preço'],
+        e['Quantidade'],
+        e['Taxa de corretagem']
+    )
+    lista_operacoes = dados_df.apply(conversao, axis=1)
+    return lista_operacoes
+
+
+def agrupa_operacoes_por_mes(df_operacoes: pd.DataFrame) -> List[pd.DataFrame]:
+    meses = df_operacoes.apply(lambda e: e.data.month).unique()
+    lista_meses = []
+    for mes in meses:
+        mascara_bin = df_operacoes.apply(lambda e: e.data.month == mes)
+        lista_meses.append(df_operacoes[mascara_bin])
+    return lista_meses
+
+
+def main():
+    dados_df = pd.read_csv('stocks-dataset.csv')
+    lista_acoes = ListaAcoes(dados_df['Ação'])
+    dados_df = pre_processa_df(dados_df, lista_acoes)
+    operacoes_df = cria_df_de_operacoes(dados_df)
+    lista_operacoes_por_mes = agrupa_operacoes_por_mes(operacoes_df)
+
+    colunas = [
+        'Mês', 
+        'Valor total de compra (reais)',
+        'Valor total de venda (reais)',
+        'Rendimento absoluto bruto (reais)',
+        'Imposto devido (reais)',
+        'Rendimento absoluto líquido (reais)'
+    ]
+    valores_saida = []
+    for lista_operacoes in lista_operacoes_por_mes:
+        mes_atual = lista_operacoes.iloc[0].data.month
+        calculadora = CalculadoraImposto(lista_acoes, mes_atual, lista_operacoes)
+        imposto_no_mes = calculadora.retorna_imposto()
+        ram = calculadora.retorna_ram()
+        total_compras = calculadora.retorna_total_compras()
+        total_vendas = calculadora.retorna_total_vendas()
+        rendimento_liquido = ram - imposto_no_mes
+        valores_saida.append([mes_atual, total_compras, total_vendas, ram, imposto_no_mes, rendimento_liquido])
+
+    df_saida = pd.DataFrame(valores_saida, columns=colunas)
+    df_saida.to_csv('dados_saida.csv', index=False, float_format='%.2f')
+    return 0
+
+if __name__ == '__main__':
+    main()
+
+
+# def calc_pm(pm, qm, pc, qc, tc):
+#     pm = (pm * qm + pc * qc + tc) / (qm + qc)
+#     return pm
+
+# def calc_add_qm(qm, qc):
+#     qm = qm + qc
+#     return qc
+
+# def calc_sell_qm(qm, qv):
+#     qm = qm - qv
+#     return qv 
+
+# def calc_ra(pv, pm, qv, tv):
+#     ra = (pv - pm) * qv - tv
+#     return ra
+
+# from collections import defaultdict
+# import pandas as pd
+# dados_df = pd.read_csv('stocks-dataset.csv')
+# formato_data_str = "%Y-%m-%d"
+# dados_df['Data da operação'] = pd.to_datetime(
+#         dados_df['Data da operação'], format=formato_data_str)
+# dados_df = dados_df.sort_values('Data da operação', ignore_index=True)
+# acoes = dados_df['Ação'].unique()
+
+# pm = defaultdict(lambda: defaultdict(lambda: -1))
+# qm = defaultdict(lambda: defaultdict(lambda: -1))
+# ra = defaultdict(lambda: defaultdict(lambda: []))
+# pa_acao = defaultdict(lambda: defaultdict(lambda: 0))
+# pa = defaultdict(lambda: 0)
+# ram = defaultdict(lambda: defaultdict(lambda: [0, 0]))
+# cont_mes = defaultdict(lambda: 0)
+
+# for index, op in dados_df.iterrows():
+#     acao = op['Ação']
+#     mes = op['Data da operação'].month
+#     cont_mes[mes] += 1
+
+#     # sempre calcula o qm
+#     # ordenação introduzida no python 3.6 e oficialmente incluida no 3.7
+#     def get_value(dict_ref, mes):
+#         if dict_ref[mes] < 0:
+#             last_moths = list(dict_ref.keys())
+#             last_moths.pop(-1)
+#             if len(last_moths):
+#                 mes = last_moths[-1]
         
-        return dict_ref[mes]
+#         return dict_ref[mes]
 
-    # se compra, calcula o pm
-    if op['Operação'] == 'Compra':
-        pm[acao][mes] = calc_pm(
-            get_value(pm[acao], mes),
-            get_value(qm[acao], mes),
-            op['Preço'],
-            op['Quantidade'],
-            op['Taxa de corretagem']
-        )
+#     # se compra, calcula o pm
+#     if op['Operação'] == 'Compra':
+#         pm[acao][mes] = calc_pm(
+#             get_value(pm[acao], mes),
+#             get_value(qm[acao], mes),
+#             op['Preço'],
+#             op['Quantidade'],
+#             op['Taxa de corretagem']
+#         )
 
-        qm[acao][mes] = calc_add_qm(
-            get_value(qm[acao], mes),
-            op['Quantidade']
-        )
+#         qm[acao][mes] = calc_add_qm(
+#             get_value(qm[acao], mes),
+#             op['Quantidade']
+#         )
 
-    # se venda, calcupa o ra
-    # TODO: o ra parece ser só do mês; organizar melhor
-    elif op['Operação'] == 'Venda':
-        ra[mes][acao].append(calc_ra(
-            op['Preço'],
-            get_value(pm[acao], mes),
-            op['Quantidade'],
-            op['Taxa de corretagem'])
-        )
+#     # se venda, calcupa o ra
+#     # TODO: o ra parece ser só do mês; organizar melhor
+#     elif op['Operação'] == 'Venda':
+#         ra[mes][acao].append(calc_ra(
+#             op['Preço'],
+#             get_value(pm[acao], mes),
+#             op['Quantidade'],
+#             op['Taxa de corretagem'])
+#         )
 
-        qm[acao][mes] = calc_sell_qm(
-            get_value(qm[acao], mes),
-            op['Quantidade']
-        )
+#         qm[acao][mes] = calc_sell_qm(
+#             get_value(qm[acao], mes),
+#             op['Quantidade']
+#         )
         
-        # se houve prejuizo na última venda
-        # TODO: o pa parece ser só do mês; organizar melhor (pode ser ui interessa para análises por ação)
-        if ra[mes][acao][-1] < 0:
-            ra_abs = abs(abs(ra[mes][acao][-1]))
-            pa_acao[acao][mes] += ra_abs
-            pa[mes] += abs(ra[mes][acao][-1])
-        # se não houve prejuizo
-        else:
-            ra_abs = abs(abs(ra[mes][acao][-1]))
-            pa_acao[acao][mes] -= min(ra_abs, pa_acao[acao][mes])
-            pa[mes] -= min(ra_abs, pa_acao[acao][mes])
+#         # se houve prejuizo na última venda
+#         # TODO: o pa parece ser só do mês; organizar melhor (pode ser ui interessa para análises por ação)
+#         if ra[mes][acao][-1] < 0:
+#             ra_abs = abs(abs(ra[mes][acao][-1]))
+#             pa_acao[acao][mes] += ra_abs
+#             pa[mes] += abs(ra[mes][acao][-1])
+#         # se não houve prejuizo
+#         else:
+#             ra_abs = abs(abs(ra[mes][acao][-1]))
+#             pa_acao[acao][mes] -= min(ra_abs, pa_acao[acao][mes])
+#             pa[mes] -= min(ra_abs, pa_acao[acao][mes])
     
-    # TODO: calcular o auferido médio de forma gradativa (para os gráficos)
-    if len(ra[mes][acao]):
-        ram[mes][acao][0] += ra[mes][acao][-1]
-        ram[mes][acao][1] += 1
+#     # TODO: calcular o auferido médio de forma gradativa (para os gráficos)
+#     if len(ra[mes][acao]):
+#         ram[mes][acao][0] += ra[mes][acao][-1]
+#         ram[mes][acao][1] += 1
     
-    print(list(op[['Data da operação', 'Ação', 'Operação']]))
-    print('pm', get_value(pm[acao], mes))
-    print('qm', get_value(qm[acao], mes))
-    print('ra', ra[mes][acao])
-    print('pa_acao', pa_acao[acao][mes])
-    print('pa', pa[mes])
+#     print(list(op[['Data da operação', 'Ação', 'Operação']]))
+#     print('pm', get_value(pm[acao], mes))
+#     print('qm', get_value(qm[acao], mes))
+#     print('ra', ra[mes][acao])
+#     print('pa_acao', pa_acao[acao][mes])
+#     print('pa', pa[mes])
 
-ram_total = defaultdict(lambda: 0)
-imposto_devido = defaultdict(lambda: 0)
-for mes in ram:
-    count = 0
-    for acao in ram[mes]:
-        ram_total[mes] += ram[mes][acao][0]
-        count += ram[mes][acao][1]
-    ram_total[mes] /= count
+# ram_total = defaultdict(lambda: 0)
+# imposto_devido = defaultdict(lambda: 0)
+# for mes in ram:
+#     count = 0
+#     for acao in ram[mes]:
+#         ram_total[mes] += ram[mes][acao][0]
+#         count += ram[mes][acao][1]
+#     ram_total[mes] /= count
 
-    ram_abs = abs(ram_total[mes])
-    imposto_devido[mes] = \
-        (ram_abs - min(ram_abs, pa[mes])) * 0.15
-print(ram, '\n')
-print(ram_total, '\n')
-print(imposto_devido, '\n')
+#     ram_abs = abs(ram_total[mes])
+#     imposto_devido[mes] = \
+#         (ram_abs - min(ram_abs, pa[mes])) * 0.15
+# print(ram, '\n')
+# print(ram_total, '\n')
+# print(imposto_devido, '\n')
+
+# rendimento_liquido = []
+# for key in ram_total:
+#     rendimento_liquido.append(ram_total[key] - imposto_devido[key])
+
+# import plotly.express as px
+# #import plotly.io as pio
+# #pio.renderers.default = "browser"
+
+# fig = px.bar(x=ram_total.keys(), y=rendimento_liquido, labels=dict(x='mês', y='rendimento absoluto líquido'))
+# fig.write_image("fig1.pdf")
---
+++ calculadora.py
@@ -0,0 +1,182 @@
+from typing import List
+import pandas as pd
+
+
+class TipoOperacao():
+    COMPRA: int = 0
+    VENDA: int = 1
+
+    @staticmethod
+    def converte_op_para_int(nome_op_str: str) -> int:
+        if nome_op_str.lower() == 'compra':
+            return TipoOperacao.COMPRA
+        elif nome_op_str.lower() == 'venda':
+            return TipoOperacao.VENDA
+        else:
+            raise ValueError("Operacação não conhecida!")
+
+
+class Acao():
+    def __init__(self, nome: str) -> None:
+        self.nome = nome
+        self.pm = 0.
+        self.qm = 0.
+    
+    def atualiza_qm(self, quantidade: int, tipo_op: TipoOperacao) -> None:
+        if tipo_op == TipoOperacao.COMPRA:
+            self.qm += quantidade
+        elif tipo_op == TipoOperacao.VENDA:
+            self.qm -= quantidade
+        else:
+            raise ValueError("Tipo de operacao desconhecida!")
+    
+    def atualiza_pm(self, preco: float, quantidade: int,
+        taxa_corretagem: int) -> None:
+        pm = 0
+        pm += self.pm * self.qm
+        pm += preco * quantidade + taxa_corretagem
+        pm /= (self.qm + quantidade)
+        self.pm = pm
+
+
+class ListaAcoes():
+    def __init__(self, acoes_serie:pd.Series=None) -> None:
+        self.acoes_obj = {}
+        if acoes_serie is not None:
+            acoes_str = acoes_serie.unique()
+            for acao_str in acoes_str:
+                self.acoes_obj[acao_str] = Acao(acao_str)
+    
+    def retorna_acao_por_nome(self, nome_acao: str) -> Acao:
+        if nome_acao in self.acoes_obj:
+            acao = self.acoes_obj[nome_acao]
+        else:
+            acao = None
+
+        return acao
+
+
+class Operacao():
+    def __init__(self, data: pd.Timestamp, tipo: TipoOperacao,
+        acao: Acao, preco: float, quantidade: int,
+        taxa_corretagem: int) -> None:
+        self.data = data
+        self.tipo = tipo
+        self.preco = preco
+        self.quantidade = quantidade
+        self.taxa_corretagem = taxa_corretagem
+        self.acao = acao
+
+        self.ra = None
+    
+    def retorna_ra(self) -> float:
+        self.calcula_ra() # Se não calculou, será calculado antes de retornar
+        return self.ra
+    
+    def retorna_preco(self) -> float:
+        return self.preco
+    
+    def retorna_tipo(self) -> TipoOperacao:
+        return self.tipo
+    
+    def retorna_quantidade(self) -> float:
+        return self.quantidade
+    
+    def calcula_ra(self) -> None:
+        if self.ra is None:
+            if self.tipo == TipoOperacao.COMPRA:
+                self.acao.atualiza_pm(self.preco, self.quantidade, self.taxa_corretagem)
+                self.ra = 0. # não há RA em caso de op de compra. Define como zero.
+            elif self.tipo == TipoOperacao.VENDA:
+                ra = (self.preco - self.acao.pm) * self.quantidade - self.taxa_corretagem
+                self.ra = ra
+            self.acao.atualiza_qm(self.quantidade, self.tipo)
+
+    
+class CalculadoraImposto():
+    def __init__(
+        self, 
+        lista_acoes: List[Acao], 
+        mes_atual: int, 
+        lista_operacoes: List[Operacao]
+        ) -> None:
+
+        self.lista_acoes = lista_acoes
+        self.lista_operacoes = lista_operacoes
+        self.mes_atual = mes_atual
+        self.ram = None
+        self.pa = None
+        self.total_compra = None
+        self.total_venda = None
+        self.imposto_devido = None
+        self.rendimento_liquido = None
+
+    def retorna_lista_operacoes(self) -> List[Operacao]:
+        return self.lista_operacoes
+
+    def retorna_ram(self) -> float:
+        self.calcula_ram()
+        return self.ram
+    
+    def retorna_pa(self) -> float:
+        self.calcula_pa()
+        return self.pa
+
+    def retorna_imposto(self) -> float:
+        self.calcula_imposto()
+        return self.imposto_devido
+    
+    def retorna_total_vendas(self) -> float:
+        self.calcula_total_venda()
+        return self.total_venda
+
+    def retorna_total_compras(self) -> float:
+        self.calcula_total_compra()
+        return self.total_compra
+
+    def calcula_pa(self) -> None:
+        if self.pa is None:
+            pa = 0.
+            for operacao in self.lista_operacoes:
+                operacao.calcula_ra()
+                ra = operacao.retorna_ra()
+                if ra < 0:
+                    pa += abs(ra)
+                else:
+                    pa -= min(ra, pa)
+            self.pa = pa
+        
+    def _calcula_total(self, tipo_op_desejado: TipoOperacao) -> float:
+        total = 0.
+        for operacao in self.lista_operacoes:
+                tipo = operacao.retorna_tipo()
+                if tipo == tipo_op_desejado:
+                    valor_unidade = operacao.retorna_preco()
+                    quantidade = operacao.retorna_quantidade()
+                    valor = valor_unidade * quantidade
+                    total += valor
+        return total
+
+    def calcula_total_compra(self) -> None:
+        if self.total_compra is None:
+            total_compra = self._calcula_total(TipoOperacao.COMPRA)
+            self.total_compra = total_compra
+    
+    def calcula_total_venda(self) -> None:
+        if self.total_venda is None:
+            total_venda = self._calcula_total(TipoOperacao.VENDA)
+            self.total_venda = total_venda
+
+    def calcula_ram(self) -> None:
+        if self.ram is None:
+            ram = 0.
+            for operacao in self.lista_operacoes:
+                operacao.calcula_ra()
+                ra = operacao.retorna_ra()
+                ram += ra
+            self.ram = ram
+    
+    def calcula_imposto(self) -> None:
+        self.calcula_ram()
+        self.calcula_pa()
+        self.imposto_devido = (self.ram - min(self.ram, self.pa)) * 0.15
